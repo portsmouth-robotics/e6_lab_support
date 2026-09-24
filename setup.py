b9b2c59e88974ca3be7421eb1009299986cf6f84
Will Keeble <william.keeble@port.ac.uk>
2026-09-24 16:26:12 +0100

    added maintainer email

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='dobot',
-    maintainer_email='dobot@todo.todo',
+    maintainer_email='technologylabs@port.ac.uk',
     description='TODO: Package description',
     license='TODO: License declaration',
     extras_require={
